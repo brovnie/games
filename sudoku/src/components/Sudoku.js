a2f88406504056aa888f280cd4c25b8793496c04
brovnie-ubuntu <maarl.deenis@gmail.com>
2022-12-02 13:59:26 +0100

    Sudoku solution generator - finished

--- sudoku/src/components/Sudoku.js
+++ sudoku/src/components/Sudoku.js
@@ -1,7 +1,7 @@
 import Input from "./UI/Input";
 import Button from "./UI/Button";
 import style from "./Sudoku.module.css";
-import { useState} from 'react';
+import { useState } from 'react';
 
 const DUMMYSUDOKU = "9715..842..69...1....8.2..95.....79...76.83...28.....57..1.5....4...91..819..7254";
 
@@ -14,41 +14,14 @@ const Sudoku = () => {
 
     const updateSudokuNumbersHandler = id => e => {
         e.preventDefault();
+        
         const value = e.target.value;
         if( (value > 0 && value < 10) || value === "") {
-            const newSudokuArray = [...sudokuNumbers];
-            newSudokuArray[id] = value;
-            setSudokuNumbers(newSudokuArray);
+            const updatedSudokuNumbers = [...sudokuNumbers];
+            updatedSudokuNumbers[id] = value;
+            setSudokuNumbers(updatedSudokuNumbers);
         }
     }
-
-
-    /*
-        const showSolutionHandler = (e) => {
-        e.preventDefault();
-        const sudokuString = sudokuNumbers.map(sudokuNumber => {
-            if(sudokuNumber === "") {
-                return '.';
-            }
-            return sudokuNumber;
-        }).join("");
-       // const sudokuSolution = getSudokuSolutuion(sudokuString);
-       // return sudokuSolution;
-               try {
-       fetch("http://0.0.0.0:9090/http://127.0.0.1:5000", {
-            method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-            body: JSON.stringify({
-                sudoku: ["9715..842..69...1....8.2..95.....79...76.83...28.....57..1.5....4...91..819..7254"]
-            })
-       })} catch (error) {
-            console.log(error.message);
-        }
-    }        
-    
-    */
 
     const showSolutionHandler = (e) => {
         const sudokuString = sudokuNumbers.map(sudokuNumber => {
@@ -57,12 +30,12 @@ const Sudoku = () => {
             }
             return sudokuNumber;
         }).join("");
-        const sudokuSolution = getSudokuSolutuion(sudokuString);
-        console.log(sudokuSolution);
+
+        const sudokuSolution = getSudokuSolution(sudokuString);
         return sudokuSolution;
     }
 
-    const getSudokuSolutuion = async (sudokuString) => {
+    const getSudokuSolution = async (sudokuString) => {
         try {
             const response = await fetch("http://localhost:9090/http://127.0.0.1:5000", {
                 method: "POST",
@@ -70,7 +43,7 @@ const Sudoku = () => {
                     "Content-Type": "application/json"
                 },
                 body: JSON.stringify({
-                    sudoku: ["9715..842..69...1....8.2..95.....79...76.83...28.....57..1.5....4...91..819..7254"]
+                    sudoku: [sudokuString]
                 })
             });
             
@@ -79,12 +52,13 @@ const Sudoku = () => {
             }
 
             const data = await response.json();
-            const solution =  data.data[0].solution;
-            return solution;
+            const solution =  data.data[0].solution.split("");
+            setSudokuNumbers(solution);
+            
         } catch (error) {
             console.log(error.message);
-        }
     }
+}
 
     return (
         <div>
